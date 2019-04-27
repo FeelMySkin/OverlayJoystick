fed06f928b4062b72be5174738b9f09c27cc4853
FeelMySkin <zetsuboulevel@gmail.com>
2019-04-27 18:13:21 +0300

    Update1

--- OverlayJoystick_WinForms/OverlayJoystick_WinForms/Overlay.Designer.cs
+++ OverlayJoystick_WinForms/OverlayJoystick_WinForms/Overlay.Designer.cs
@@ -29,23 +29,51 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 82);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Overlay
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Overlay";
             this.Text = "Overlay";
             this.Load += new System.EventHandler(this.Overlay_Load);
@@ -58,5 +86,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- OverlayJoystick_WinForms/OverlayJoystick_WinForms/Overlay.cs
+++ OverlayJoystick_WinForms/OverlayJoystick_WinForms/Overlay.cs
@@ -45,17 +45,30 @@ namespace OverlayJoystick_WinForms
 
         private void Overlay_VisibleChanged(object sender, EventArgs e)
         {
+            
             if (!this.Visible) return;
             label1.Text = new Random((int)DateTime.Now.ToBinary()).Next().ToString();
 
             int initialStyle = GetWindowLong(this.Handle, -20);
-            SetWindowLong(this.Handle, -20, initialStyle | 0x80000 | 0x20);
+            //SetWindowLong(this.Handle, -20, initialStyle | 0x80000 | 0x20);
+            SetWindowLong(this.Handle, -20, initialStyle | 0x08000000 | 0x8);
 
             IntPtr handle = FindWindow(null, WINDOW_NAME);
             GetWindowRect(handle, out rect);
             this.Size = new Size(rect.right - rect.left, rect.bottom - rect.top);
             this.Top = rect.top;
             this.Left = rect.left;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.BackColor == Color.Wheat) this.BackColor = Color.Gray;
+            else this.BackColor = Color.Wheat;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send(Keys.A.ToString());
         }
     }
 }
